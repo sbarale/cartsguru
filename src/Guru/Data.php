c3ebf855289bef1af7d91b64dac09ba405d3f8ba
Sebastian Barale <sebasinar@gmail.com>
2017-07-27 10:11:00 -0300

    Passing parameters on the constructor

--- src/Guru/Data.php
+++ src/Guru/Data.php
@@ -15,6 +15,16 @@ class Data
      * A simple wrapper to make our lives easier. Yes I know it's silly
      * but for this case it'll help a lot.
      */
+
+    public function __construct($data = null)
+    {
+        if (!is_null($data)) {
+            foreach (get_object_vars((object)$data) as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+    }
+
     public function __get($name)
     {
         if (!property_exists(__CLASS__, $name)) {
